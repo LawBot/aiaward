ca5d315afdb70cfbec975b7af5ef928a760d0c50
Ben Chen <jinerben@gmail.com>
2017-11-06 21:49:33 +0100

    Changed structure of program such that main program does not contain logical code anymore. Also changed package name for main

--- src/cn/com/xiaofabo/scia/aiaward/fileprocessor/ApplicationDocReader.java
+++ src/cn/com/xiaofabo/scia/aiaward/fileprocessor/ApplicationDocReader.java
@@ -5,20 +5,45 @@
  */
 package cn.com.xiaofabo.scia.aiaward.fileprocessor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 /**
  *
  * @author cgx82
  */
-public class ApplicationDocReader implements InputFileReader{
+public class ApplicationDocReader implements InputFileReader {
+
     static Logger logger = Logger.getLogger(ApplicationDocReader.class.getName());
-    
-    public ApplicationDocReader(){
+    private String docText;
+
+    public ApplicationDocReader() {
         PropertyConfigurator.configure("log/config.txt");
-        logger.info("Constructor of ApplicationDocReader");
+        logger.trace("Constructor of ApplicationDocReader");
+    }
+
+    public String read(String inputPath) throws IOException {
+        // Newer version word documents
+        try {
+            XWPFDocument docx = new XWPFDocument(new FileInputStream(inputPath));
+            System.out.println(docx.getParagraphs().size());
+            XWPFWordExtractor we = new XWPFWordExtractor(docx);
+            docText = we.getText();
+        }
+        // Old version word documents
+        catch (org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException e) {
+            HWPFDocument doc = new HWPFDocument(new FileInputStream(inputPath));
+            WordExtractor we = new WordExtractor(doc);
+            docText = we.getText();
+        }
+        return docText;
     }
 }
--- src/cn/com/xiaofabo/scia/aiaward/main/AIAwardGenerator.java
+++ src/cn/com/xiaofabo/scia/aiaward/main/AIAwardGenerator.java
@@ -3,13 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aiawardgenerator;
+package cn.com.xiaofabo.scia.aiaward.main;
 
 import cn.com.xiaofabo.scia.aiaward.fileprocessor.ApplicationDocReader;
 import cn.com.xiaofabo.scia.aiaward.fileprocessor.AwardDocGenerator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
@@ -23,11 +26,15 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
  * @author cgx82
  */
 public class AIAwardGenerator {
+    
+    private static Logger logger = Logger.getLogger(AIAwardGenerator.class.getName());
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        PropertyConfigurator.configure("log/config.txt");
+        logger.info("AI Award Generator program started...");
         //Blank Document
 //        XWPFDocument document = new XWPFDocument();
 //
@@ -60,24 +67,23 @@ public class AIAwardGenerator {
 //        out.close();
 //        System.out.println("fontstyle.docx written successully");
 
-        String inputPath = "D:\\OneDrive\\小法博科技\\产品\\AI裁决书\\SHEN DP2014088-未生成\\SHEN DP2014088\\仲裁资料\\申请人\\13282_AI-仲裁申请书.doc";
+        String inputPath = "test\\SHEN DT20170003\\仲裁资料\\申请人\\12281_AI-仲裁申请书.docx";
         String outputPath = "test\\SHEN DT20170003\\testoutput\\award.docx";
 
-        //XWPFDocument docx = null;
+        ApplicationDocReader adr = new ApplicationDocReader();
         try {
-            XWPFDocument docx = new XWPFDocument(new FileInputStream(inputPath));
-            //using XWPFWordExtractor Class
-            XWPFWordExtractor we = new XWPFWordExtractor(docx);
-            System.out.println(we.getText());
-        } catch (org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException e) {
-            HWPFDocument doc = new HWPFDocument(new FileInputStream(inputPath));
-            WordExtractor we = new WordExtractor(doc);
-            System.out.println(we.getText());
-            //System.out.println("Input file has a format which is too old!");
+            System.out.println(adr.read(inputPath));
+        } catch (IOException e) {
+            logger.fatal("Cannot find application file: " + inputPath);
+            logger.fatal("Please make sure the file exists!");
+            logger.fatal("PROGRAM ABORTED!!!");
+            return;
         }
 
         AwardDocGenerator awardGen = new AwardDocGenerator(outputPath);
         awardGen.generateAwardDoc();
+        
+        logger.info("AI Award Generator program finished successfully");
     }
 
 }
